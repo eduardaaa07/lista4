4a87e32b9fc7243af81f60e67533b9b81fb78426
CJ301679X <CJ301679X@LAB01-PC33>
2023-04-12 11:27:26 -0300

    Adicionar arquivos de projeto.

---
+++ lista4/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        //    exercicio 4
+        //    int num, num2, num3;
+        //    Console.WriteLine("Entre com um número: ");
+        //    num = int.Parse(Console.ReadLine());
+        //    num2 = num / 2;
+        //    num3 = num % 2;
+        //    if (num3 == 0 && num2 * 2 > 10)
+        //        Console.WriteLine("Mensagem 1");
+        //    else
+        //        Console.WriteLine("Mensagem 2");
+        //    Console.ReadKey();
+
+        //    exercicio 5
+        //        using System;
+
+        //     class Program
+        //{
+        //    static void Main(string[] args)
+        //    {
+        //        double v, t, s;
+
+        //        Console.WriteLine("Cálculo da posição final (MRU)");
+        //        Console.WriteLine("Digite a velocidade do objeto (m/s): ");
+        //        v = double.Parse(Console.ReadLine());
+
+        //        Console.WriteLine("Digite o tempo de movimento (s): ");
+        //        t = double.Parse(Console.ReadLine());
+
+        //        s = v * t;
+        //        Console.WriteLine("A posição final do objeto é: " + s + " m");
+        //        Console.ReadKey();
+
+        //        exercicio 6
+
+        //        using System;
+
+        //       class Program
+        //    {
+        //        static void Main(string[] args)
+        //        {
+        //            Random rnd = new Random();
+        //            int num = rnd.Next(1, 101);
+        //            Console.WriteLine("Número gerado: " + num);
+
+        //            if (num % 3 == 0 && num % 5 == 0 && num % 10 == 0)
+        //            {
+        //                Console.WriteLine("Divisível por 3, 5 e 10");
+        //            }
+        //            else if (num % 3 == 0 && num % 5 == 0)
+        //            {
+        //                Console.WriteLine("Divisível por 3 e 5");
+        //            }
+        //            else if (num % 3 == 0)
+        //            {
+        //                Console.WriteLine("Somente divisível por 3");
+        //            }
+        //            else if (num % 5 == 0)
+        //            {
+        //                Console.WriteLine("Somente divisível por 5");
+        //            }
+        //            else
+        //            {
+        //                Console.WriteLine("Não é divisível por 3 ou 5 ou 10");
+        //                Console.ReadKey();
+
+
+                    }
+                }
+}
